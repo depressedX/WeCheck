2a170d5af9b1abaca58c6b3cd00f9739519c7ffb
quyans@sina.com <quyans@sina.com>
2018-08-08 14:19:22 +0800

    上传有问题

--- src/components/RegisterForm.vue
+++ src/components/RegisterForm.vue
@@ -33,14 +33,24 @@
                     class="upload-demo"
                     drag
                     action="https://jsonplaceholder.typicode.com/posts/"
-                    multiple>
+                    multiple
+                    on-change="beforeUpload()"
+                    id="aaa"
+                    >
                 <i class="el-icon-upload"></i>
                 <div class="el-upload__text">将文件拖到此处，或<em>点击上传</em></div>
                 <div class="el-upload__tip" slot="tip">只能上传jpg/png文件，且不超过500kb</div>
             </el-upload>
         </el-form-item>
 
+
     </el-form>
+
+
+
+
+
+
 
 
 </template>
@@ -95,7 +105,15 @@
             },
             resetForm(formName) {
                 this.$refs[formName].resetFields();
+            },
+
+            getFile(){
+                var file = document.getElementById("aaa").files[0];
+                console.log(file);
+                console.log(file['name']);
+
             }
+
         }
     }
 
