d0865c7219bf74a204e883c71fc518034f2339b7
lph <yuri8023>
2018-09-22 02:54:34 +0800

    。。。

--- src/pages/management/App.vue
+++ src/pages/management/App.vue
@@ -1,14 +1,21 @@
 <template>
     <div id="app">
-
+        <QRCodeScanner @detected="alert"/>
         <router-view/>
     </div>
 </template>
 
 
 <script>
-
-
+    import QRCodeScanner from "@/components/QRCodeScanner";
+    export default {
+        components: {QRCodeScanner},
+        methods:{
+            alert(c){
+                alert(c)
+            }
+        }
+    }
 </script>
 <style lang="scss">
 
